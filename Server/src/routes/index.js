58904d6fc5c5e6103f4c4c2b0611dae8446b2153
goranjako <goranjako@gmail.com>
2023-05-15 22:12:25 +0200

    src

---
+++ Server/src/routes/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import contactroute from "./contactroute";
+import authroute from "./authroute";
+
+router.use("/contact", contactroute);
+router.use("/auth", authroute);
+
+export default router;
