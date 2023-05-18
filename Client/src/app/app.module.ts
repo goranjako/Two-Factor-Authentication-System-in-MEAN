14129e742173a317f74fc6139b33d67980a718cf
goranjako <goranjako@gmail.com>
2023-05-18 21:14:50 +0200

    init

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,20 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { Interceptor } from './shared/interceptor.interceptor';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, RouterModule,
+    FormsModule, ReactiveFormsModule, HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    NgxSpinnerModule.forRoot(),
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MaterialModule,
+    SweetAlert2Module.forRoot()
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
